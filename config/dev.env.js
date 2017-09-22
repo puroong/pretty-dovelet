df20228c67b5b0a8e2aba094647f3087652810aa
puroong <skfk0135@gmail.com>
2017-09-22 11:19:36 +0900

    Login page done

--- config/dev.env.js
+++ config/dev.env.js
@@ -3,5 +3,5 @@ var prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_HOST: '"http://localhost:8080/"'
+  API_HOST: '"http://localhost:8081/"'
 })
--- src/config/router.js
+++ src/config/router.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Login from '@/spa/login/index.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    }
   ]
 })
