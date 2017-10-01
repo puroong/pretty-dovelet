223dc25b16bb31d0b2ab4cfa768a91fd8d10e243
puroong <skfk0135@gmail.com>
2017-10-02 01:28:59 +0900

    Delete all

--- config/prod.env.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  NODE_ENV: '"production"',
-  // TODO: change production api host
-  API_HOST: '"production api host"'
-}
--- src/commons/api.js
+++
@@ -1,9 +0,0 @@
-require('es6-promise').polyfill()
-
-import axios from 'axios'
-
-let APIv1 = axios.create({
-  baseURL: `${process.env.API_HOST}`
-})
-
-export { APIv1 }
